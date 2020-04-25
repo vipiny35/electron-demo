4c66cf0e0b47f1af08c347d440ec721ec90c6f39
Vipin Yadav <vipiny35@gmail.com>
2020-04-25 11:37:36 +0530

    Angular version

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
 const electron = require('electron');
 const path = require('path');
 const url = require('url');
+const { ipcMain } = require('electron');
 
-// SET ENV
-process.env.NODE_ENV = 'development';
-
-const { app, BrowserWindow, Menu } = electron;
+const { app, BrowserWindow } = electron;
 
 app.allowRendererProcessReuse = false;
 
 app.on('ready', function () {
 
   const mainWindow = new BrowserWindow({
+    width: 920,
     height: 600,
-    width: 800,
     webPreferences: {
       nodeIntegration: true,
       webviewTag: true
@@ -23,61 +21,13 @@ app.on('ready', function () {
   mainWindow.removeMenu();
 
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, 'angular/index.html'),
     protocol: 'file:',
     slashes: true
   }));
 
-  // mainWindow.webContents.openDevTools();
-
   mainWindow.on('closed', function () {
     app.quit();
   });
-
-  const template = [
-    {
-      label: 'View 1',
-      click: function () {
-        mainWindow.loadURL(url.format({
-          pathname: path.join(__dirname, 'angular/index.html'),
-          protocol: 'file:',
-          slashes: true
-        }));
-      }
-    },
-    {
-      label: 'View 2',
-      click: function () {
-        mainWindow.loadURL(url.format({
-          pathname: path.join(__dirname, 'new-me/index.html'),
-          protocol: 'file:',
-          slashes: true
-        }));
-      }
-    },
-    {
-      label: 'View 3',
-      click: function () {
-        mainWindow.loadURL("https://www.webtiara.com/blog");
-      }
-    },
-    {
-      label: '+',
-      submenu: [
-        {
-          label: 'View 1'
-        },
-        {
-          label: 'View 2'
-        },
-        {
-          label: 'View 3'
-        }
-      ]
-    }
-  ];
-
-  const menu = Menu.buildFromTemplate(template);
-  Menu.setApplicationMenu(menu);
 
 });
